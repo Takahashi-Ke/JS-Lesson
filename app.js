84042a7426a4dd4171863b670b9b93cf2bba90a8
高橋健太郎 <takahashikentaro@takahashikentarounoMacBook-Air.local>
2020-10-28 13:44:19 +0900

    [Finish]JS-7

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-// $(document).ready(function () {
-//   $('body').html('<h1>Hello jQuery!</h1>');
+// $(function (){
+//   $('.bg1').on('click',function(){
+//     $('.bg1').slideUp();
+//   });
+//   $('.bg2').on('click',function(){
+//     $('.bg2').slideUp();
+//   });
+//   $('.bg3').on('click',function(){
+//     $('.bg3').slideUp();
+//   });
+//   $('.bg4').on('click',function(){
+//     $('.bg4').slideUp();
+//   });
 // });
-// $(document).ready(function () {
-//   $('body').html('<h1>Hello jQuery!!</h1>');
-// });
-
-// $(function(){
-  // jQueryプログラムの内容
-// });
-
+// 以下、thisを使用した省略型
+// コード上では何をクリックしたのかわかりづらいと言うデメリットがある
 $(function (){
-  $('.box1').on('click',function (){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function (){
-    $('.box1').removeClass('box1-ext');
+  $('.box1').on('click',function(){
+    $(this).slideUp();
   });
 });
 
-
+$(function (){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red')
+  });
+});
